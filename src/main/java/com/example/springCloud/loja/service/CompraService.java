ec36ec17e77f926532c3d7f7a0fe4fa077e38c08
Tathiana Ianelli Oliveira <tathiana.oliveira@b2wdigital.com>
2020-06-24 11:32:03 -0300

    terceiro commit

--- src/main/java/com/example/springCloud/loja/service/CompraService.java
+++ src/main/java/com/example/springCloud/loja/service/CompraService.java
@@ -1,10 +1,6 @@
 package com.example.springCloud.loja.service;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,36 +9,44 @@ import com.example.springCloud.loja.dto.CompraDto;
 import com.example.springCloud.loja.dto.InfoFornecedorDto;
 import com.example.springCloud.loja.dto.InfoPedidoDto;
 import com.example.springCloud.loja.model.Compra;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 
 @Service
 public class CompraService {
 	
+	private static final Logger LOG = LoggerFactory.getLogger(CompraService.class);
+
 	@Autowired
-	private RestTemplate client; //com ribon e não com feign
-	
+	private RestTemplate client; // com ribon e não com feign
+
 	@Autowired
 	private FornecedorClient fornecedorClient; // com feign
 
 	public Compra realizaCompra(CompraDto compra) {
-		
-		/* primeira forma: 
-		ResponseEntity<InfoFornecedorDto> exchange =   client.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(),
-				HttpMethod.GET, null, InfoFornecedorDto.class);
-		
-		System.out.println(exchange.getBody().getEndereco());
+
+		String estado = compra.getEndereco().getEstado();
+		/*
+		 * primeira forma: ResponseEntity<InfoFornecedorDto> exchange =
+		 * client.exchange("http://fornecedor/info/"+compra.getEndereco().getEstado(),
+		 * HttpMethod.GET, null, InfoFornecedorDto.class);
+		 * 
+		 * System.out.println(exchange.getBody().getEndereco());
 		 */
 
-		// segunda forma: 
-		
-		
+		// segunda forma:
+
+		LOG.info("buscando informações do fornecedor de {}", estado );
 		InfoFornecedorDto info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+		
+		LOG.info("realizando um pedido");
 		InfoPedidoDto pedido = fornecedorClient.realizaCompra(compra.getItens());
-		
-		Compra compraFeita = new Compra(pedido.getId(),pedido.getTempoDePreparo(),compra.getEndereco().toString());
+
+		Compra compraFeita = new Compra(pedido.getId(), pedido.getTempoDePreparo(), compra.getEndereco().toString());
 		return compraFeita;
-		
+
 	}
 
 }
